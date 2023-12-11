d44fcf5a0da50f89dfffc377c0ccb1cc4430dee7
semihkenan <149376572+semihkenan@users.noreply.github.com>
2023-12-11 18:31:22 +0100

    commit in real french2

--- src/be/vdab/git/HelloGit.java
+++ src/be/vdab/git/HelloGit.java
@@ -3,6 +3,7 @@ package be.vdab.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello GİTT!");
-        System.out.println("bonjour git");
+        System.out.println("Bon apetit");
     }
 }
+
