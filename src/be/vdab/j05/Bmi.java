26a62fb645a9a849643eb34de4bb23b1b564d742
semihkenan <149376572+semihkenan@users.noreply.github.com>
2023-12-12 10:54:14 +0100

    bmi berekening

---
+++ src/be/vdab/j05/Bmi.java
@@ -0,0 +1,12 @@
+package be.vdab.j05;
+
+public class Bmi {
+    public static void main(String[] args) {
+        float gewicht = 75.7F;
+        float lengte = 1.70F;
+        float bmi;
+
+        bmi= gewicht /(lengte*lengte);
+        System.out.println("de bodyIndex is "+ bmi);
+    }
+}
